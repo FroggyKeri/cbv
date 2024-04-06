c796b2b125dc53c09ee1670823172eacfdec6b41
FroggyKeri <151474673+FroggyKeri@users.noreply.github.com>
2024-04-06 22:11:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printHelp (){
+#define MAX_LINE 2000
+
+void printHelp()
+{ // Print Help to CLI
 
     printf("--help              Hilfeinformation wird ausgeben; Programm terminiert\n");
     printf("-s n                Angabe der ersten auszugebenen (n-te) Zeile\n");
@@ -26,41 +29,109 @@ void printHelp (){
     printf("--version           Versionsinformation des Programms wird ausgegeben\n");
 }
 
-void printVersion (){
+void printVersion()
+{ // Print Version
 
     printf("cvb version 1.0\n");
 }
 
-void printVerboseMode() {
-
-
+void error(char *err)
+{ // Error Function to print to stderr
+    fprintf(stderr, "%s\n", err);
 }
 
-void printEndOfLine() {
-
-
-}
-
-void readFileQuiet() {
-
-
-}
-
-int main(int argc, char *argv[]){
-FILE *file;
-
-
-for (int i = 1; i < argc; i++){
-    if (strcmp(argv[i], "--help") == 0){
-        printHelp();
-        return 0;
-    } else if (strcmp(argv[i],"--version") == 0){
-        printVersion();
-        return 0;
+void printLines(FILE *file, int start, int end)// Core function to print Lines from start to end
+{ 
+    if(file == NULL){
+        printf("AAAAAAAAAAAAAAAAAAAAAAAA");
     }
-    
+    char lineBuffer[MAX_LINE]; //array to save the current line to
+    int currentLine = start; //iterator for line were currently at
+    while (fgets(lineBuffer, MAX_LINE, file) != NULL) //ersetze mit fseek damit wir nicht durchs ganze file müssen?
+    {
+        if (currentLine <= end)
+        {
+            printf("%s", lineBuffer);
+        }
+        else
+        {
+            break;
+        }
+        currentLine++;
+        printf("%d", currentLine);
+    }
 }
 
+void readFileQuiet()
+{
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    int start = 1;
+    int end = -1;
+    char *filename = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--help") == 0)
+        {
+            printHelp();
+            return 0;
+        }
+        else if (strcmp(argv[i], "--version") == 0)
+        {
+            printVersion();
+            return 0;
+        }
+        else if (strcmp(argv[i], "-s") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                start = strtol(argv[i + 1], NULL, 10);
+            }
+            else
+            {
+                error("-s benötigt ein Argument");
+            }
+        }
+        else if (strcmp(argv[i], "-e") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                end = strtol(argv[i + 1], NULL, 10);
+            }
+            else
+            {
+                error("-e benötigt ein Argument");
+            }
+        }
+        else
+        {
+            filename = argv[argc];
+        }
+    }
+
+    if (filename == NULL || strcmp(filename, "-") == 0)
+    {
+        file = stdin;
+    }
+    else
+    {
+        file = fopen(filename, "r");
+        if (file == NULL)
+        {
+            error("Fehler beim Öffnen der Datei.");
+        }
+    }
+
+    printLines(file, start, end);
+
+    if (file != stdin)
+    {
+        fclose(file);
+    }
 
     return 0;
 }
